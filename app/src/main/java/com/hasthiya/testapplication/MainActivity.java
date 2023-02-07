fda877bdd64f795d83121d949028a222f12cfae9
Kasun M.Weerasekara <98303334+Kasunm12@users.noreply.github.com>
2023-02-07 13:35:10 +0530

    create to register onclick form

--- app/src/main/java/com/hasthiya/testapplication/MainActivity.java
+++ app/src/main/java/com/hasthiya/testapplication/MainActivity.java
@@ -21,14 +21,30 @@ public class MainActivity extends AppCompatActivity {
 //        edtTxtName.getText().toString();
     }
 
-    public void onBtnClick (View view){
+//    public void onBtnClick (View view){
+//
+//        TextView textHello = findViewById(R.id.textMessage);
+//        EditText edtTxtName = findViewById(R.id.editTxtName);
+////        edtTxtName.getText().toString();
+////        textHello.setText("Hello");
+////        textHello.setText(edtTxtName.getText().toString());
+//
+//        textHello.setText("Hello" +edtTxtName.getText().toString());
+//    }
 
-        TextView textHello = findViewById(R.id.textMessage);
-        EditText edtTxtName = findViewById(R.id.editTxtName);
-//        edtTxtName.getText().toString();
-//        textHello.setText("Hello");
-//        textHello.setText(edtTxtName.getText().toString());
+    public void onRegisterBtnClick (View view){
 
-        textHello.setText("Hello" +edtTxtName.getText().toString());
+        TextView txtFirstName = findViewById(R.id.txtFirstName);
+        TextView txtLastName = findViewById(R.id.txtLastName);
+        TextView txtEmail = findViewById(R.id.txtEmail);
+
+        EditText edtTxtFirstName = findViewById(R.id.edtTxtFirstName);
+        EditText edtTxtLastName = findViewById(R.id.edtTxtLastName);
+        EditText edtTxtEmail = findViewById(R.id.edtTxtEmail);
+
+        txtFirstName.setText("First Name : " +edtTxtFirstName.getText().toString());
+        txtLastName.setText("First Name : " +edtTxtLastName.getText().toString());
+        txtEmail.setText("First Name : " +edtTxtEmail.getText().toString());
+
     }
 }
